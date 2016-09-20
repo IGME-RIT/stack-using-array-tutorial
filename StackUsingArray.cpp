3cfda076c9ed268afd594a4905c6bb08d9765dbe
Andrew Litfin <arl8315@rit.edu>
2016-09-20 16:32:08 -0400

    Setup tutorial for ATLAS
    Add .gitignore, CMakeLists.txt, igme_thumbnail.png, setup, and setup.cmd
    Update igme_config.yml
    Code formatting changes in StackUsingArray.cpp (mostly extraneous spacing)

--- StackUsingArray.cpp
+++ StackUsingArray.cpp
@@ -2,16 +2,17 @@
 #include <cassert>
 #include <iostream>
 #include <math.h>
+
 using namespace std;
+
 typedef struct {
 	int* elems;
 	int top = 0;
 	int allocLength;
+} Stack;
 
-}Stack;
-
-void createStack(Stack* s) {
-	
+void createStack(Stack* s)
+{
 	s->top = 0;
 	s->allocLength = 4;
 	s->elems = (int*)malloc(sizeof(int) * 4);
@@ -21,27 +22,27 @@ void createStack(Stack* s) {
 void pushStack(Stack* s, int value)
 {
 	if (s->top == s->allocLength) {
-
 		s->allocLength *= 2;
 		s->elems = (int*)realloc(s->elems, s->allocLength* sizeof(int));
 	//	Doubling strategy
 	}
 	s->elems[s->top] = value;
 	s->top++;
-
 }
+
 int popStack(Stack* s)
 {
 	assert(s->top > 0);
 	s->top--;
 	return s->elems[s->top];
-
 }
+
 void deleteStack(Stack *s)
 {
 	free(s->elems);
 	s->top = 0;
 }
+
 void printStack(Stack* s)
 {
 	for (int i = 0; i < s->top; i++)
@@ -49,17 +50,16 @@ void printStack(Stack* s)
 		cout << s->elems[i] << " ";
 	}
 	cout << endl;
-
-
 }
 
 int main() {
-
 	Stack s;
 	createStack(&s);
+
 	for (int i = 0; i < 10; i++) {
 		pushStack(&s, i);
 	}
+
 	printStack(&s);
 	cout << "Pop: " << popStack(&s) << endl;
 	printStack(&s);
@@ -68,5 +68,8 @@ int main() {
 
 	cout << "Stack disposed" << endl;
 	deleteStack(&s);
-	printStack(&s);
+
+	cout << "\nPress Enter to continue . . . ";
+	cin.ignore();
+	return 0;
 }
